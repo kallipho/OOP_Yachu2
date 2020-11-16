c68b29a2fa78500e8ea3fb2caecba597f805ce60
kallipho <sshs19112@gmail.com>
2020-11-16 11:45:45 +0900

    Player.java 수정

--- Yachu_Local2/src/Player.java
+++ Yachu_Local2/src/Player.java
@@ -7,9 +7,15 @@ public class Player {
 		this.rollNum = 3;
 	}
 	
-	//public void rollDices() //주사위들을 굴림
-	//public void keep(boolean[] selectedList) //selectedList에 따라 dices.kept 조정
-
+	public void rollDices() { //주사위를 굴림
+		for (Dice x : dice){
+			x.Roll();
+		}
+	}
+	
+	public void select(String Comb) {
+		//TODO 내용 추가 필요
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
