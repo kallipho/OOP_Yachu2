cafeb4e016469ca2913a2b8d24486bc402346c39
kallipho <sshs19112@gmail.com>
2020-11-16 11:41:56 +0900

    Dice.java, Player.java 수정

--- Yachu_Local2/src/Dice.java
+++ Yachu_Local2/src/Dice.java
@@ -3,15 +3,16 @@ public class Dice {
 	protected int eyes;
 	protected boolean kept; //True면 킵 된거 
 	
-	Dice(){
-		this.eyes = 0;
-		this.kept = false;
-	}
-	
 	protected void Roll() {
 		if(!kept) {
 			Random rand = new Random();
 			eyes = 1 + rand.nextInt(5); //rand.nextInt(5)는 0~5까지 Int를 생성시키고 1을 더하면 1~6이 됨.
 		}
+	}
+	
+	Dice(){
+		Random rand = new Random();
+		this.eyes = 1 + rand.nextInt(5); //생성할 때 랜덤으로 초기화
+		this.kept = false;
 	}
 }
--- Yachu_Local2/src/Player.java
+++ Yachu_Local2/src/Player.java
@@ -1,8 +1,12 @@
 public class Player {
-
-	//private Scoreboard board = new Scoreboard() //개인 스코어보드
-	//private Dice[] dices = new dices[5] //주사위 저장
-	//public int rollNum;
+	private Scoreboard SB = new Scoreboard();
+	private Dice[] dice = new Dice[5];
+	public int rollNum;
+	
+	Player() {
+		this.rollNum = 3;
+	}
+	
 	//public void rollDices() //주사위들을 굴림
 	//public void keep(boolean[] selectedList) //selectedList에 따라 dices.kept 조정
 
