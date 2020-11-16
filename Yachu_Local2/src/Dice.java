7f35deea0162bc91140868afaf7b1be23b75e26f
kallipho <sshs19112@gmail.com>
2020-11-16 11:36:54 +0900

    Main 삭제 했음

--- Yachu_Local2/src/Dice.java
+++ Yachu_Local2/src/Dice.java
@@ -14,10 +14,4 @@ public class Dice {
 			eyes = 1 + rand.nextInt(5); //rand.nextInt(5)는 0~5까지 Int를 생성시키고 1을 더하면 1~6이 됨.
 		}
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
