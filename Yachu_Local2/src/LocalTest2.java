8171261822dc07dfec46649984f106e49759a0a6
Dongmin Choi <57855480+kallipho@users.noreply.github.com>
2020-11-16 09:36:07 +0900

    Pull Test용 커밋 체인지

--- Yachu_Local2/src/LocalTest2.java
+++ Yachu_Local2/src/LocalTest2.java
@@ -5,6 +5,7 @@ public class LocalTest2 {
 		System.out.println("Helloworld");
 		System.out.println("�젣媛� �솗�떎�븯寃� �븣�젮�뱶由ш쿋�뒿�땲�떎");
 		System.out.println("I sing one time");
+		System.out.println("sysout for pull test");
 	}
 
 }
