6e2417691df6266b92748c509885d3f7b86894af
kallipho <sshs19112@gmail.com>
2020-11-16 11:33:46 +0900

    Dice.java 변수, Roll() 구현 완료

--- Yachu_Local2/src/Dice.java
+++ Yachu_Local2/src/Dice.java
@@ -1,7 +1,19 @@
+import java.util.Random;
 public class Dice {
-	//protected int eyes //현재 눈
-	//protected boolean kept //선택된 상태인지
-	//protected void Roll() //주사위 한개 굴리기
+	protected int eyes;
+	protected boolean kept; //True면 킵 된거 
+	
+	Dice(){
+		this.eyes = 0;
+		this.kept = false;
+	}
+	
+	protected void Roll() {
+		if(!kept) {
+			Random rand = new Random();
+			eyes = 1 + rand.nextInt(5); //rand.nextInt(5)는 0~5까지 Int를 생성시키고 1을 더하면 1~6이 됨.
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
