60969e1aaf9fc95ebae787c0dc2ed36f26790487
phokalli <sshs19112@gmail.com>
2020-11-29 01:36:52 +0900

    주석 추가

--- Yachu_Local2/src/Dice.java
+++ Yachu_Local2/src/Dice.java
@@ -4,7 +4,7 @@ public class Dice {
 	private int Eye; //주사위 위에 현재 향해있는 눈
 	private boolean IsLock; //주사위가 Keep되어있는지(Keep된 주사위는 다시 굴리지 않음)
 	
-	public void Roll() {
+	public void Roll() { //주사위를 다시 굴림. Eye는 private이기 때문에 직접 굴리지 않는 이상 변경할 수 없음
 		if(!IsLock) {
 			Random rand = new Random();
 			Eye = 1 + rand.nextInt(5);
